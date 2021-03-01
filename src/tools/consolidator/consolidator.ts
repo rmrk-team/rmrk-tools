825578dd33d692fe9b945fdb48a229f4e086bb3b
Yuri <yuripetusko@gmail.com>
2021-03-01 22:53:33 +0000

    Cleanup consolidator a little bit further, fix eslint warnings

--- src/tools/consolidator/consolidator.ts
+++ src/tools/consolidator/consolidator.ts
@@ -1,16 +1,23 @@
 import JsonAdapter from "./adapters/json";
 import { Collection as C100 } from "../../rmrk1.0.0/classes/collection";
-import { NFT as N100, Reactionmap } from "../../rmrk1.0.0/classes/nft";
+import { NFT as N100 } from "../../rmrk1.0.0/classes/nft";
 import { ChangeIssuer } from "../../rmrk1.0.0/classes/changeissuer";
 import { Send } from "../../rmrk1.0.0/classes/send";
 import { Emote } from "../../rmrk1.0.0/classes/emote";
 import { Change } from "../../rmrk1.0.0/changelog";
 import { deeplog } from "../utils";
-import * as fs from "fs";
-
 import { decodeAddress } from "@polkadot/keyring";
 import { u8aToHex } from "@polkadot/util";
 import { Remark } from "./remark";
+// import * as fs from "fs";
+
+enum OP_TYPES {
+  MINT = "MINT",
+  MINTNFT = "MINTNFT",
+  SEND = "SEND",
+  EMOTE = "EMOTE",
+  CHANGEISSUER = "CHANGEISSUER",
+}
 
 export class Consolidator {
   private adapter: JsonAdapter;
@@ -26,26 +33,40 @@ export class Consolidator {
   private findExistingCollection(id: string) {
     return this.collections.find((el) => el.id === id);
   }
-  private mint(remark: Remark): boolean {
-    // A new collection was created
-    console.log("Instantiating collection");
-    const op_type = "MINT";
+  private updateInvalidCalls(op_type: OP_TYPES, remark: Remark) {
     const invalidCallBase: Partial<InvalidCall> = {
       op_type,
       block: remark.block,
       caller: remark.caller,
     };
+    return function update(
+      this: Consolidator,
+      object_id: string,
+      message: string
+    ) {
+      this.invalidCalls.push({
+        ...invalidCallBase,
+        object_id,
+        message,
+      } as InvalidCall);
+    };
+  }
+  private mint(remark: Remark): boolean {
+    // A new collection was created
+    console.log("Instantiating collection");
+    const invalidate = this.updateInvalidCalls(OP_TYPES.MINT, remark).bind(
+      this
+    );
     const c = C100.fromRemark(remark.remark, remark.block);
 
     if (typeof c === "string") {
       // console.log(
       //   "Collection was not instantiated OK from " + remark.remark
       // );
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Dead before instantiation: ${c}`,
-        object_id: remark.remark,
-      } as InvalidCall);
+      invalidate(
+        remark.remark,
+        `[${OP_TYPES.MINT}] Dead before instantiation: ${c}`
+      );
       return true;
     }
 
@@ -54,22 +75,20 @@ export class Consolidator {
     const id = C100.generateId(u8aToHex(pubkey), c.symbol);
 
     if (this.findExistingCollection(c.id)) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: "[MINT] Attempt to mint already existing collection",
-        object_id: c.id,
-      } as InvalidCall);
+      invalidate(
+        c.id,
+        `[${OP_TYPES.MINT}] Attempt to mint already existing collection`
+      );
       return true;
     }
 
     if (id.toLowerCase() !== c.id.toLowerCase()) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `Caller's pubkey ${u8aToHex(
+      invalidate(
+        c.id,
+        `Caller's pubkey ${u8aToHex(
           pubkey
-        )} (${id}) does not match generated ID`,
-        object_id: c.id,
-      } as InvalidCall);
+        )} (${id}) does not match generated ID`
+      );
       return true;
     }
 
@@ -80,40 +99,34 @@ export class Consolidator {
   private mintNFT(remark: Remark): boolean {
     // A new NFT was minted into a collection
     console.log("Instantiating nft");
-    const op_type = "MINTNFT";
-    const invalidCallBase: Partial<InvalidCall> = {
-      op_type,
-      block: remark.block,
-      caller: remark.caller,
-    };
+    const invalidate = this.updateInvalidCalls(OP_TYPES.MINTNFT, remark).bind(
+      this
+    );
     const n = N100.fromRemark(remark.remark, remark.block);
 
     if (typeof n === "string") {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Dead before instantiation: ${n}`,
-        object_id: remark.remark,
-      } as InvalidCall);
+      invalidate(
+        remark.remark,
+        `[${OP_TYPES.MINTNFT}] Dead before instantiation: ${n}`
+      );
       return true;
     }
 
     const nftParent = this.findExistingCollection(n.collection);
     if (!nftParent) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `NFT referencing non-existant parent collection ${n.collection}`,
-        object_id: n.getId(),
-      } as InvalidCall);
+      invalidate(
+        n.getId(),
+        `NFT referencing non-existant parent collection ${n.collection}`
+      );
       return true;
     }
 
     n.owner = nftParent.issuer;
     if (remark.caller != n.owner) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `Attempted issue of NFT in non-owned collection. Issuer: ${nftParent.issuer}, caller: ${remark.caller}`,
-        object_id: n.getId(),
-      } as InvalidCall);
+      invalidate(
+        n.getId(),
+        `Attempted issue of NFT in non-owned collection. Issuer: ${nftParent.issuer}, caller: ${remark.caller}`
+      );
       return true;
     }
 
@@ -130,19 +143,17 @@ export class Consolidator {
     });
 
     if (existsCheck) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Attempt to mint already existing NFT`,
-        object_id: n.getId(),
-      } as InvalidCall);
+      invalidate(
+        n.getId(),
+        `[${OP_TYPES.MINTNFT}] Attempt to mint already existing NFT`
+      );
       return true;
     }
     if (n.owner === "") {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Somehow this NFT still doesn't have an owner.`,
-        object_id: n.getId(),
-      } as InvalidCall);
+      invalidate(
+        n.getId(),
+        `[${OP_TYPES.MINTNFT}] Somehow this NFT still doesn't have an owner.`
+      );
       return true;
     }
     this.nfts.push(n);
@@ -153,18 +164,14 @@ export class Consolidator {
     // An NFT was sent to a new owner
     console.log("Instantiating send");
     const send = Send.fromRemark(remark.remark);
-    const op_type = "SEND";
-    const invalidCallBase: Partial<InvalidCall> = {
-      op_type,
-      block: remark.block,
-      caller: remark.caller,
-    };
+    const invalidate = this.updateInvalidCalls(OP_TYPES.SEND, remark).bind(
+      this
+    );
     if (typeof send === "string") {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Dead before instantiation: ${send}`,
-        object_id: remark.remark,
-      } as InvalidCall);
+      invalidate(
+        remark.remark,
+        `[${OP_TYPES.SEND}] Dead before instantiation: ${send}`
+      );
       return true;
     }
 
@@ -181,21 +188,19 @@ export class Consolidator {
     });
 
     if (!nft) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Attempting to send non-existant NFT ${send.id}`,
-        object_id: send.id,
-      } as InvalidCall);
+      invalidate(
+        send.id,
+        `[${OP_TYPES.SEND}] Attempting to send non-existant NFT ${send.id}`
+      );
       return true;
     }
 
     // Check if allowed to issue send - if owner == caller
     if (nft.owner != remark.caller) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Attempting to send non-owned NFT ${send.id}, real owner: ${nft.owner}`,
-        object_id: send.id,
-      } as InvalidCall);
+      invalidate(
+        send.id,
+        `[${OP_TYPES.SEND}] Attempting to send non-owned NFT ${send.id}, real owner: ${nft.owner}`
+      );
       return true;
     }
 
@@ -215,33 +220,25 @@ export class Consolidator {
     // An EMOTE reaction has been sent
     console.log("Instantiating emote");
     const emote = Emote.fromRemark(remark.remark);
-    const op_type = "EMOTE";
-    const invalidCallBase: Partial<InvalidCall> = {
-      op_type,
-      block: remark.block,
-      caller: remark.caller,
-    };
+    const invalidate = this.updateInvalidCalls(OP_TYPES.EMOTE, remark).bind(
+      this
+    );
     if (typeof emote === "string") {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Dead before instantiation: ${emote}`,
-        object_id: remark.remark,
-      } as InvalidCall);
+      invalidate(
+        remark.remark,
+        `[${OP_TYPES.EMOTE}] Dead before instantiation: ${emote}`
+      );
       return true;
     }
     const target = this.nfts.find((el) => el.getId() === emote.id);
     if (!target) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Attempting to emote on non-existant NFT ${emote.id}`,
-        object_id: emote.id,
-      } as InvalidCall);
+      invalidate(
+        emote.id,
+        `[${OP_TYPES.EMOTE}] Attempting to emote on non-existant NFT ${emote.id}`
+      );
       return true;
     }
-    const index = target.reactions[emote.unicode].indexOf(
-      remark.caller,
-      0
-    );
+    const index = target.reactions[emote.unicode].indexOf(remark.caller, 0);
     if (index > -1) {
       target.reactions[emote.unicode].splice(index, 1);
     } else {
@@ -254,39 +251,34 @@ export class Consolidator {
     // The ownership of a collection has changed
     console.log("Instantiating an issuer change");
     const ci = ChangeIssuer.fromRemark(remark.remark);
-    const op_type = "CHANGEISSUER";
-    const invalidCallBase: Partial<InvalidCall> = {
-      op_type,
-      block: remark.block,
-      caller: remark.caller,
-    };
+    const invalidate = this.updateInvalidCalls(
+      OP_TYPES.CHANGEISSUER,
+      remark
+    ).bind(this);
     if (typeof ci === "string") {
       // console.log(
       //   "ChangeIssuer was not instantiated OK from " + remark.remark
       // );
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `[${op_type}] Dead before instantiation: ${ci}`,
-        object_id: remark.remark,
-      } as InvalidCall);
+      invalidate(
+        remark.remark,
+        `[${OP_TYPES.CHANGEISSUER}] Dead before instantiation: ${ci}`
+      );
       return true;
     }
     const coll = this.collections.find((el: C100) => el.id === ci.id);
     if (!coll) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `This ${op_type} remark is invalid - no such collection with ID ${ci.id} found before block ${remark.block}!`,
-        object_id: ci.id,
-      } as InvalidCall);
+      invalidate(
+        ci.id,
+        `This ${OP_TYPES.CHANGEISSUER} remark is invalid - no such collection with ID ${ci.id} found before block ${remark.block}!`
+      );
       return true;
     }
 
     if (remark.caller != coll.issuer) {
-      this.invalidCalls.push({
-        ...invalidCallBase,
-        message: `Attempting to change issuer of collection ${ci.id} when not issuer!`,
-        object_id: ci.id,
-      } as InvalidCall);
+      invalidate(
+        ci.id,
+        `Attempting to change issuer of collection ${ci.id} when not issuer!`
+      );
       return true;
     }
 
@@ -299,7 +291,7 @@ export class Consolidator {
     } as Change);
 
     coll.issuer = ci.issuer;
-    return false
+    return false;
   }
 
   public consolidate(): void {
@@ -314,41 +306,44 @@ export class Consolidator {
             continue;
           }
           break;
+
         case "MINTNFT":
           if (this.mintNFT(remark)) {
             continue;
           }
           break;
+
         case "SEND":
           if (this.send(remark)) {
             continue;
           }
           break;
+
         case "BUY":
           // An NFT was bought after being LISTed
-
           break;
+
         case "LIST":
           // An NFT was listed for sale
-
           break;
+
         case "EMOTE":
           if (this.emote(remark)) {
             continue;
           }
           break;
+
         case "CHANGEISSUER":
           if (this.changeIssuer(remark)) {
             continue;
           }
-
           break;
+
         default:
           console.error(
             "Unable to process this remark - wrong type: " +
               remark.interaction_type
           );
-          continue;
       }
     }
     deeplog(this.nfts);
