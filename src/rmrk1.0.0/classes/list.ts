5bea5671665d421d18b3e8c3e17e68bb842b8211
Bruno Skvorc <bruno@skvorc.me>
2021-03-05 16:13:50 +0100

    List interaction

---
+++ src/rmrk1.0.0/classes/list.ts
@@ -0,0 +1,33 @@
+export class List {
+  price: BigInt;
+  id: string;
+  static V = "1.0.0";
+
+  constructor(id: string, price: BigInt) {
+    this.price = price;
+    this.id = id;
+  }
+
+  static fromRemark(remark: string): List | string {
+    const exploded = remark.split("::");
+    try {
+      if (exploded[0] != "RMRK")
+        throw new Error("Invalid remark - does not start with RMRK");
+      if (exploded[2] != List.V)
+        throw new Error(
+          `Version mismatch. Is ${exploded[2]}, should be ${List.V}`
+        );
+      if (exploded[1] != "LIST")
+        throw new Error("The op code needs to be LIST, is " + exploded[1]);
+
+      if (undefined === exploded[3] || undefined == exploded[4]) {
+        throw new Error("Cound not find ID or price");
+      }
+    } catch (e) {
+      console.error(e.message);
+      console.log(`SEND error: full input was ${remark}`);
+      return e.message;
+    }
+    return new List(exploded[3], BigInt(exploded[4]));
+  }
+}
--- src/tools/consolidator/consolidator.ts
+++ src/tools/consolidator/consolidator.ts
@@ -346,7 +346,7 @@ export class Consolidator {
     }
     deeplog(this.nfts);
     deeplog(this.collections);
-    console.log(this.invalidCalls);
+    //console.log(this.invalidCalls);
   }
 }
 
