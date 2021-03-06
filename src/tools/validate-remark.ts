2dfa1bdeb04f507daadd1d8908212e54f27015a3
Yuri <yuripetusko@gmail.com>
2021-03-06 23:39:48 +0000

    Refactor validation of consolidator remarks and add some unit tests (more to follow)

--- src/tools/validate-remark.ts
+++ src/tools/validate-remark.ts
@@ -9,6 +9,7 @@ import {
   any,
   optional,
   type,
+  is,
 } from "superstruct";
 import { getRemarkData } from "./utils";
 
@@ -38,18 +39,51 @@ const NFTStruct = type({
   metadata: optional(pattern(string(), new RegExp("^(https?|ipfs)://.*$"))),
 });
 
+const IsBigInt = define("BigInt", (value) => {
+  try {
+    if (!is(value, string())) {
+      return false;
+    }
+
+    const priceBigInt = BigInt(parseInt(value));
+    return typeof priceBigInt === "bigint";
+  } catch (error) {
+    return false;
+  }
+});
+
+const LISTStruct = type({
+  id: string(),
+  price: IsBigInt,
+});
+
+const SENDStruct = type({
+  id: string(),
+  recipient: string(),
+});
+
+const EMOTEStruct = type({
+  id: string(),
+  unicode: string(),
+});
+
+const CHANGEISSUERStruct = type({
+  id: string(),
+  issuer: string(),
+});
+
 export const validateBase = (remark: string, opType: OP_TYPES) => {
   const [prefix, op_type, version] = remark.split("::");
-  if (prefix !== PREFIX) {
+  if (prefix.toUpperCase() !== PREFIX) {
     throw new Error("Invalid remark - does not start with RMRK");
+  }
+  if (op_type !== opType) {
+    throw new Error(`The op code needs to be ${opType}, but it is ${op_type}`);
   }
   if (version !== VERSION) {
     throw new Error(
       `This remark was issued under version ${version} instead of ${VERSION}`
     );
-  }
-  if (op_type !== opType) {
-    throw new Error(`The op code needs to be ${opType}, but it is ${op_type}`);
   }
 };
 
@@ -63,8 +97,8 @@ export const validateCollection = (remark: string): any => {
     return assert(obj, CollectionStruct);
   } catch (error) {
     console.log("StructError is:", error);
-    return new Error(
-      error?.message || "Something went wrrong during remark validation"
+    throw new Error(
+      error?.message || "Something went wrong during remark validation"
     );
   }
 };
@@ -79,8 +113,68 @@ export const validateNFT = (remark: string): any => {
     return assert(obj, NFTStruct);
   } catch (error) {
     console.log("StructError is:", error);
-    return new Error(
-      error?.message || "Something went wrrong during remark validation"
+    throw new Error(
+      error?.message || "Something went wrong during remark validation"
+    );
+  }
+};
+
+export const validateList = (remark: string): any => {
+  // With array destructuring it's important to not remove unused destructured variables, as order is important
+  const [_prefix, _op_type, _version, id, price] = remark.split("::");
+
+  try {
+    validateBase(remark, OP_TYPES.LIST);
+    return assert({ id, price }, LISTStruct);
+  } catch (error) {
+    console.log("StructError is:", error);
+    throw new Error(
+      error?.message || "Something went wrong during remark validation"
+    );
+  }
+};
+
+export const validateSend = (remark: string): any => {
+  // With array destructuring it's important to not remove unused destructured variables, as order is important
+  const [_prefix, _op_type, _version, id, recipient] = remark.split("::");
+
+  try {
+    validateBase(remark, OP_TYPES.SEND);
+    return assert({ id, recipient }, SENDStruct);
+  } catch (error) {
+    console.log("StructError is:", error);
+    throw new Error(
+      error?.message || "Something went wrong during remark validation"
+    );
+  }
+};
+
+export const validateEmote = (remark: string): any => {
+  // With array destructuring it's important to not remove unused destructured variables, as order is important
+  const [_prefix, _op_type, _version, id, unicode] = remark.split("::");
+
+  try {
+    validateBase(remark, OP_TYPES.EMOTE);
+    return assert({ id, unicode }, EMOTEStruct);
+  } catch (error) {
+    console.log("StructError is:", error);
+    throw new Error(
+      error?.message || "Something went wrong during remark validation"
+    );
+  }
+};
+
+export const validateChangeIssuer = (remark: string): any => {
+  // With array destructuring it's important to not remove unused destructured variables, as order is important
+  const [_prefix, _op_type, _version, id, issuer] = remark.split("::");
+
+  try {
+    validateBase(remark, OP_TYPES.CHANGEISSUER);
+    return assert({ id, issuer }, CHANGEISSUERStruct);
+  } catch (error) {
+    console.log("StructError is:", error);
+    throw new Error(
+      error?.message || "Something went wrong during remark validation"
     );
   }
 };
--- test/utils/validate-remark.test.ts
+++ test/utils/validate-remark.test.ts
@@ -2,32 +2,43 @@ import test from "ava";
 import {
   validateBase,
   validateCollection,
+  validateNFT,
 } from "../../src/tools/validate-remark";
-import { mintRemarkValidMocks } from "../mocks/remark-mocks";
+import {
+  mintRemarkValidMocks,
+  validMintNFTRemarkEvent,
+} from "../mocks/remark-mocks";
+import { OP_TYPES } from "../../src/tools/constants";
 
 test("validation: validateBase", (t) => {
-  t.plan(5);
   t.notThrows(() =>
-    validateCollection(
-      'RMRK::MINT::1.0.0::{"name"%3A"Foo"%2C"max"%3A5%2C"issuer"%3A"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"%2C"symbol"%3A"FOO"%2C"id"%3A"d43593c715a56da27d-FOO"%2C"metadata"%3A"https%3A%2F%2Fsome.url"}'
+    validateBase(
+      'RMRK::MINT::1.0.0::{"name"%3A"Foo"%2C"max"%3A5%2C"issuer"%3A"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"%2C"symbol"%3A"FOO"%2C"id"%3A"d43593c715a56da27d-FOO"%2C"metadata"%3A"https%3A%2F%2Fsome.url"}',
+      OP_TYPES.MINT
     )
   );
   t.throws(() =>
-    validateCollection(
-      'BRB::MINT::1.0.0::{"name"%3A"Foo"%2C"max"%3A5%2C"issuer"%3A"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"%2C"symbol"%3A"FOO"%2C"id"%3A"d43593c715a56da27d-FOO"%2C"metadata"%3A"https%3A%2F%2Fsome.url"}'
+    validateBase(
+      'BRB::MINT::1.0.0::{"name"%3A"Foo"%2C"max"%3A5%2C"issuer"%3A"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"%2C"symbol"%3A"FOO"%2C"id"%3A"d43593c715a56da27d-FOO"%2C"metadata"%3A"https%3A%2F%2Fsome.url"}',
+      OP_TYPES.MINT
     )
   );
   t.throws(() =>
-    validateCollection(
-      'RMRK::CLINT::1.0.0::{"name"%3A"Foo"%2C"max"%3A5%2C"issuer"%3A"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"%2C"symbol"%3A"FOO"%2C"id"%3A"d43593c715a56da27d-FOO"%2C"metadata"%3A"https%3A%2F%2Fsome.url"}'
+    validateBase(
+      'RMRK::CLINT::1.0.0::{"name"%3A"Foo"%2C"max"%3A5%2C"issuer"%3A"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"%2C"symbol"%3A"FOO"%2C"id"%3A"d43593c715a56da27d-FOO"%2C"metadata"%3A"https%3A%2F%2Fsome.url"}',
+      OP_TYPES.MINT
     )
   );
   t.throws(() =>
-    validateCollection(
-      'RMRK::MINT::0.0.0::{"name"%3A"Foo"%2C"max"%3A5%2C"issuer"%3A"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"%2C"symbol"%3A"FOO"%2C"id"%3A"d43593c715a56da27d-FOO"%2C"metadata"%3A"https%3A%2F%2Fsome.url"}'
+    validateBase(
+      'RMRK::MINT::0.0.0::{"name"%3A"Foo"%2C"max"%3A5%2C"issuer"%3A"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"%2C"symbol"%3A"FOO"%2C"id"%3A"d43593c715a56da27d-FOO"%2C"metadata"%3A"https%3A%2F%2Fsome.url"}',
+      OP_TYPES.MINT
     )
   );
-  t.throws(() => validateCollection("RMRK::MINT::1.0.0"));
+  t.notThrows(() => validateBase("RMRK::MINT::1.0.0", OP_TYPES.MINT));
+  t.notThrows(() =>
+    validateBase("RMRK::MINT::1.0.0::FOO::BAR::BAZ", OP_TYPES.MINT)
+  );
 });
 
 test("validation: validateCollection", (t) => {
@@ -35,4 +46,72 @@ test("validation: validateCollection", (t) => {
   mintRemarkValidMocks.forEach((remark) => {
     t.notThrows(() => validateCollection(remark));
   });
+});
+
+test("validation: validateNFT", (t) => {
+  t.notThrows(() => validateNFT(validMintNFTRemarkEvent));
+
+  t.throws(
+    () => {
+      validateNFT(
+        'RMRK::XXINTNFT::1.0.0::{"collection"%3A"test"%2C"sn"%3A"0000000000000001"%2C"transferable"%3A1%2C"name"%3A"VIP Invitation %231"%2C"metadata"%3A"ipfs%3A%2F%2Fipfs%2FQmQ2Q57PVpaP8QvWvvH9kfn1CdCY49pcv1AaLBjDwS2p4g"%2C"currentOwner"%3A"HPSgWwpjnMe9oyBq4t2dA3dRTU8PwDAU32q6E76xjFDDrEX"%2C"instance"%3A"VIP_INVITATION_1"}'
+      );
+    },
+    { instanceOf: Error }
+  );
+
+  const error = t.throws(
+    () => {
+      validateNFT(
+        'RMRK::MINTNFT::1.0.0::{"collection"%3A22%2C"sn"%3A"0000000000000001"%2C"transferable"%3A1%2C"name"%3A"VIP Invitation %231"%2C"metadata"%3A"ipfs%3A%2F%2Fipfs%2FQmQ2Q57PVpaP8QvWvvH9kfn1CdCY49pcv1AaLBjDwS2p4g"%2C"currentOwner"%3A"HPSgWwpjnMe9oyBq4t2dA3dRTU8PwDAU32q6E76xjFDDrEX"%2C"instance"%3A"VIP_INVITATION_1"}'
+      );
+    },
+    { instanceOf: Error }
+  );
+
+  t.is(
+    error.message,
+    "At path: collection -- Expected a string, but received: 22"
+  );
+
+  t.throws(
+    () => {
+      validateNFT(
+        'RMRK::MINTNFT::1.0.0::{"collection"%3A"category"%2C"sn"%3A111%2C"transferable"%3A1%2C"name"%3A"VIP Invitation %231"%2C"metadata"%3A"ipfs%3A%2F%2Fipfs%2FQmQ2Q57PVpaP8QvWvvH9kfn1CdCY49pcv1AaLBjDwS2p4g"%2C"currentOwner"%3A"HPSgWwpjnMe9oyBq4t2dA3dRTU8PwDAU32q6E76xjFDDrEX"%2C"instance"%3A"VIP_INVITATION_1"}'
+      );
+    },
+    { instanceOf: Error }
+  );
+
+  t.throws(
+    () => {
+      validateNFT(
+        'RMRK::MINTNFT::1.0.0::{"collection"%3A"category"%2C"sn"%3A"0000000000000001"%2C"transferable"%3A"1"%2C"name"%3A"VIP Invitation %231"%2C"metadata"%3A"ipfs%3A%2F%2Fipfs%2FQmQ2Q57PVpaP8QvWvvH9kfn1CdCY49pcv1AaLBjDwS2p4g"%2C"currentOwner"%3A"HPSgWwpjnMe9oyBq4t2dA3dRTU8PwDAU32q6E76xjFDDrEX"%2C"instance"%3A"VIP_INVITATION_1"}'
+      );
+    },
+    { instanceOf: Error }
+  );
+
+  t.throws(
+    () => {
+      validateNFT(
+        'RMRK::MINTNFT::1.0.0::{"collection"%3A"category"%2C"foo"%3A"0000000000000001"%2C"transferable"%3A1%2C"name"%3A"VIP Invitation %231"%2C"metadata"%3A"ipfs%3A%2F%2Fipfs%2FQmQ2Q57PVpaP8QvWvvH9kfn1CdCY49pcv1AaLBjDwS2p4g"%2C"currentOwner"%3A"HPSgWwpjnMe9oyBq4t2dA3dRTU8PwDAU32q6E76xjFDDrEX"%2C"instance"%3A"VIP_INVITATION_1"}'
+      );
+    },
+    { instanceOf: Error }
+  );
+
+  const metadataError = t.throws(
+    () => {
+      validateNFT(
+        'RMRK::MINTNFT::1.0.0::{"collection"%3A"category"%2C"sn"%3A"0000000000000001"%2C"transferable"%3A1%2C"name"%3A"VIP Invitation %231"%2C"metadata"%3A"hpfs%3A%2F%2Fipfs%2FQmQ2Q57PVpaP8QvWvvH9kfn1CdCY49pcv1AaLBjDwS2p4g"%2C"currentOwner"%3A"HPSgWwpjnMe9oyBq4t2dA3dRTU8PwDAU32q6E76xjFDDrEX"%2C"instance"%3A"VIP_INVITATION_1"}'
+      );
+    },
+    { instanceOf: Error }
+  );
+
+  t.is(
+    metadataError.message,
+    'At path: metadata -- Expected a string matching `/^(https?|ipfs):\\/\\/.*$/` but received "hpfs://ipfs/QmQ2Q57PVpaP8QvWvvH9kfn1CdCY49pcv1AaLBjDwS2p4g"'
+  );
 });
