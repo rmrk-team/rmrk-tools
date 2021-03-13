8b9a4c45b4db9c1076bf315c97c662cffff31a6b
Yuri <yuripetusko@gmail.com>
2021-03-13 20:37:37 +0000

    Remove dist folder

--- dist/browser/polkadot-utils.d.ts
+++
@@ -1,4 +0,0 @@
-export * as extensionDapps from "@polkadot/extension-dapp";
-export * as utilCrypto from "@polkadot/util-crypto";
-export * as util from "@polkadot/util";
-export * as api from "@polkadot/api";
--- dist/src/rmrk1.0.0/classes/buy.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Buy {
-    price: BigInt;
-    id: string;
-    constructor(id: string, price: BigInt);
-    static fromRemark(remark: string): Buy | string;
-}
