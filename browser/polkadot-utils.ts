e909def66424255e6bd66749ba59aca54738aea2
Yuri <yuripetusko@gmail.com>
2021-03-13 19:29:39 +0000

    Build browser bundle with correct exports

---
+++ browser/polkadot-utils.ts
@@ -0,0 +1,4 @@
+export * as extensionDapps from "@polkadot/extension-dapp";
+export * as utilCrypto from "@polkadot/util-crypto";
+export * as util from "@polkadot/util";
+export * as api from "@polkadot/api";
