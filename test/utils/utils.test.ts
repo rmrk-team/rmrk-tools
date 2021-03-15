35b4b2b473847644b0b283a86b4a33b8073fc71c
abzen <abovezenith@gmail.com>
2021-03-15 22:07:18 +0000

    add tests for stringIsAValidUrl util

--- test/utils/utils.test.ts
+++ test/utils/utils.test.ts
@@ -1,8 +1,25 @@
 import { blocks } from "../mocks/blocks";
-import { getRemarksFromBlocks } from "../../src/tools/utils";
+import { getRemarksFromBlocks, stringIsAValidUrl } from "../../src/tools/utils";
 
 describe("utils: getRemarksFromBlocks", () => {
   it("should return remarks from blocks", () => {
     expect(getRemarksFromBlocks(blocks)).toMatchSnapshot();
+  });
+});
+
+const testUrls: Record<string, string> = {
+  url: 'https://rmrk.app/',
+  wrongUrl: 'wrong url',
+}
+
+describe("utils: stringIsAValidUrl", () => {
+  it("should check if string is a URL and return true", () => {
+    expect(stringIsAValidUrl(testUrls.url)).toBeTruthy();
+  });
+});
+
+describe("utils: stringIsAValidUrl", () => {
+  it("should check if string is a URL and return false", () => {
+    expect(stringIsAValidUrl(testUrls.wrongUrl)).toBeFalsy();
   });
 });
