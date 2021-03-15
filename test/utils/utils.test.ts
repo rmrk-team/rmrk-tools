b64a653bba5fa21781a6c95375455496e88f53f1
abzen <abovezenith@gmail.com>
2021-03-15 22:47:53 +0000

    prettier

--- test/utils/utils.test.ts
+++ test/utils/utils.test.ts
@@ -1,16 +1,22 @@
 import { blocks } from "../mocks/blocks";
-import { getRemarksFromBlocks, stringIsAValidUrl } from "../../src/tools/utils";
+import {
+  getRemarksFromBlocks,
+  stringIsAValidUrl,
+  prefixToArray,
+} from "../../src/tools/utils";
 
+// Test getRemarksFromBlocks
 describe("utils: getRemarksFromBlocks", () => {
   it("should return remarks from blocks", () => {
     expect(getRemarksFromBlocks(blocks)).toMatchSnapshot();
   });
 });
 
+// Test stringIsAValidUrl
 const testUrls: Record<string, string> = {
-  url: 'https://rmrk.app/',
-  wrongUrl: 'wrong url',
-}
+  url: "https://rmrk.app/",
+  wrongUrl: "wrong url",
+};
 
 describe("utils: stringIsAValidUrl", () => {
   it("should check if string is a URL and return true", () => {
@@ -21,5 +27,12 @@ describe("utils: stringIsAValidUrl", () => {
 describe("utils: stringIsAValidUrl", () => {
   it("should check if string is a URL and return false", () => {
     expect(stringIsAValidUrl(testUrls.wrongUrl)).toBeFalsy();
+  });
+});
+
+// Test prefixToArray
+describe("utils: prefixToArray", () => {
+  it("should check if string is a URL and return true", () => {
+    expect(prefixToArray(testUrls.url)).toBeTruthy();
   });
 });
