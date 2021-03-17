b9f4095521eff5b1786d86d98af007dd16ce7794
Yuri <yuripetusko@gmail.com>
2021-03-17 17:58:04 +0000

    don't use commander for cli tools

--- cli/validate.ts
+++ cli/validate.ts
@@ -1,34 +1,32 @@
 #! /usr/bin/env node
-import commander from "commander";
-import { Options } from "../src/tools/types";
 import { NFT as N100 } from "../src/rmrk1.0.0/classes/nft";
 import { deeplog } from "../src/tools/utils";
 import { OP_TYPES } from "../src/tools/constants";
+import arg from "arg";
 
-export const addTo = (program: commander.CommanderStatic | typeof commander) =>
-  program
-    .command("validate")
-    .option("--remark <remark>", "The remark to validate")
-    .action(async (opts: Options) => {
-      const remark = opts.remark;
-      const exploded = remark.split("::");
-      if (exploded.length < 2) {
-        throw new Error(
-          "Invalid RMRK remark, cannot explode by double-colon (::)"
-        );
-      }
-      if (exploded[0].toUpperCase() !== "RMRK") {
-        throw new Error(
-          "This is not a RMRK remark - does not begin with RMRK/rmrk"
-        );
-      }
-      switch (exploded[1]) {
-        case OP_TYPES.MINTNFT:
-          console.log(`Identified as ${OP_TYPES.MINTNFT}`);
-          const n = N100.fromRemark(remark);
-          deeplog(n);
-          break;
-        default:
-          throw new Error(`${exploded[1]} interaction not supported`);
-      }
-    });
+const validate = async () => {
+  const args = arg({
+    // Types
+    "--remark": String, // The remark to validate
+  });
+
+  const remark = args["--remark"] || "";
+  const exploded = remark.split("::");
+  if (exploded.length < 2) {
+    throw new Error("Invalid RMRK remark, cannot explode by double-colon (::)");
+  }
+  if (exploded[0].toUpperCase() !== "RMRK") {
+    throw new Error(
+      "This is not a RMRK remark - does not begin with RMRK/rmrk"
+    );
+  }
+  switch (exploded[1]) {
+    case OP_TYPES.MINTNFT:
+      console.log(`Identified as ${OP_TYPES.MINTNFT}`);
+      const n = N100.fromRemark(remark);
+      deeplog(n);
+      break;
+    default:
+      throw new Error(`${exploded[1]} interaction not supported`);
+  }
+};
