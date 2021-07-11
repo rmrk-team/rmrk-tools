23bd11cc14c30cb844aa82a14f05148b455e2683
Yuri Gii <abovezenith@gmail.com>
2021-07-11 22:18:10 +0100

    test class base change_issuer method

--- test/2.0.0/classes/base.test.ts
+++ test/2.0.0/classes/base.test.ts
@@ -1,5 +1,5 @@
 import { cryptoWaitReady } from "@polkadot/util-crypto";
-import { createNftClassMock, createBaseMock } from "../mocks";
+import { getBobKey, createBaseMock, createNftClassMock } from "../mocks";
 
 beforeAll(async () => {
   return await cryptoWaitReady();
@@ -19,6 +19,28 @@ describe("rmrk2.0.0 Base: Base", () => {
       await baseClass.base();
     } catch (e) {
       expect(e.message).toMatch("An already existing BASE cannot be minted!");
+    }
+  });
+});
+
+describe("rmrk2.0.0 Base: Change issuer", () => {
+  it("should match snapshot", async () => {
+    const baseClass = createBaseMock(1);
+    expect(
+      await baseClass.change_issuer(getBobKey().address)
+    ).toMatchSnapshot();
+  });
+});
+
+describe("rmrk2.0.0 Base: Base", () => {
+  it("should throw error", async () => {
+    const baseClass = createBaseMock(0);
+    try {
+      await baseClass.change_issuer(getBobKey().address);
+    } catch (e) {
+      expect(e.message).toMatch(
+        "This base is new, so there's no issuer to change. If it has been deployed on chain, load the existing nft class as a new instance first, then change issuer."
+      );
     }
   });
 });
