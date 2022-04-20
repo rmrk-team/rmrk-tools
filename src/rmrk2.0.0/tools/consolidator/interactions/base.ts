0e1824839cddd0632b3a1cc7e113a111be95332f
Erebos0506 <93453130+Erebos0506@users.noreply.github.com>
2022-04-20 14:22:30 +0200

    Update base.ts

--- src/rmrk2.0.0/tools/consolidator/interactions/base.ts
+++ src/rmrk2.0.0/tools/consolidator/interactions/base.ts
@@ -1,9 +1,11 @@
 import { Remark } from "../remark";
 import { OP_TYPES } from "../../constants";
 import {Base} from "../../../classes/base";
+import { encodeAddress } from "@polkadot/keyring";
 
 export const getBaseFromRemark = (remark: Remark, ss58Format?: number) => {
-  const base = Base.fromRemark(remark.remark, remark.block, ss58Format);
+  const issuer = encodeAddress(remark.caller, ss58Format);
+  const base = Base.fromRemark(remark.remark, issuer, remark.block);
   if (typeof base === "string") {
     throw new Error(`[${OP_TYPES.BASE}] Dead before instantiation: ${base}`);
   }
