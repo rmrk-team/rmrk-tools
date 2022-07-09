c42b42b34283d2e7a6c423ab11717de4fc7e5b0e
Erebos0506 <93453130+Erebos0506@users.noreply.github.com>
2022-07-09 12:31:36 +0200

    Update base.ts

--- src/rmrk2.0.0/tools/consolidator/interactions/base.ts
+++ src/rmrk2.0.0/tools/consolidator/interactions/base.ts
@@ -7,5 +7,13 @@ export const getBaseFromRemark = (remark: Remark, ss58Format?: number) => {
   if (typeof base === "string") {
     throw new Error(`[${OP_TYPES.BASE}] Dead before instantiation: ${base}`);
   }
+  const part_ids = [];
+  for (let i = 0; i < base.parts.length; i++) {
+    if (part_ids.includes(base.parts[i].id)) {
+      throw new Error(`[${OP_TYPES.BASE}] Duplicate base part id found: ${base.parts[i].id}`);
+    } else {
+      part_ids.push(base.parts[i].id);
+    }
+  }
   return base;
 };
